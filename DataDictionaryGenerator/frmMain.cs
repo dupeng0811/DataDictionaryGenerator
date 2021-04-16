50438a73209b2d29dccb0ffd4d69b10925c06031
dupeng0811 <dupeng0811@qq.com>
2021-04-16 11:50:05 +0800

    完成生成Markdown功能

--- DataDictionaryGenerator/frmMain.cs
+++ DataDictionaryGenerator/frmMain.cs
@@ -54,25 +54,51 @@ namespace DataDictionaryGenerator
         {
             ReturnMessage retMsg = new ReturnMessage(string.Empty, true);
             dtInfo.Rows.Clear();
-            string strQry = @"SELECT TOP (100) PERCENT 
-                CASE WHEN a.colorder = 1 THEN d .name ELSE '' END AS 表名, CASE WHEN a.colorder = 1 THEN isnull(f.value, '') 
-                ELSE '' END AS 表说明, a.colorder AS 字段序号, a.name AS 字段名, CASE WHEN COLUMNPROPERTY(a.id, a.name, 'IsIdentity') 
-                = 1 THEN '√' ELSE '' END AS 标识, CASE WHEN EXISTS 
-                (SELECT 1 FROM dbo.sysindexes si INNER JOIN 
-                dbo.sysindexkeys sik ON si.id = sik.id AND si.indid = sik.indid INNER JOIN 
-                dbo.syscolumns sc ON sc.id = sik.id AND sc.colid = sik.colid INNER JOIN 
-                dbo.sysobjects so ON so.name = so.name AND so.xtype = 'PK' 
-                WHERE sc.id = a.id AND sc.colid = a.colid) THEN '√' ELSE '' END AS 主键, b.name AS 类型, a.length AS 长度, COLUMNPROPERTY(a.id, a.name, 
-                'PRECISION') AS 精度, ISNULL(COLUMNPROPERTY(a.id, a.name, 'Scale'), 0) AS 小数位数, 
-                CASE WHEN a.isnullable = 1 THEN '√' ELSE '' END AS 允许空, ISNULL(e.text, '') AS 默认值, ISNULL(g.value, '') AS 字段说明, d.crdate AS 创建时间, 
-                CASE WHEN a.colorder = 1 THEN d .refdate ELSE NULL END AS 更改时间 
-                FROM dbo.syscolumns AS a LEFT OUTER JOIN 
-                dbo.systypes AS b ON a.xtype = b.xusertype INNER JOIN 
-                dbo.sysobjects AS d ON a.id = d.id AND d.xtype = 'U'  AND d.status >= 0 LEFT OUTER JOIN 
-                dbo.syscomments AS e ON a.cdefault = e.id LEFT OUTER JOIN 
-                sys.extended_properties AS g ON a.id = g.major_id AND a.colid = g.minor_id LEFT OUTER JOIN 
-                sys.extended_properties AS f ON d.id = f.major_id AND f.minor_id = 0 
-                ORDER BY d.name, 字段序号";
+            string strQry = @"SELECT TOP (100) PERCENT d.name                  AS 表名,
+                         CASE
+                             WHEN a.colorder = 1 THEN isnull(f.value, '')
+                             ELSE '' END                                       AS 表说明,
+                         a.colorder                                            AS 字段序号,
+                         a.name                                                AS 字段名,
+                         CASE
+                             WHEN COLUMNPROPERTY(a.id, a.name, 'IsIdentity')
+                                 = 1 THEN '√'
+                             ELSE '' END                                       AS 标识,
+                         CASE
+                             WHEN EXISTS
+                                 (SELECT 1
+                                  FROM dbo.sysindexes si
+                                           INNER JOIN
+                                       dbo.sysindexkeys sik ON si.id = sik.id AND si.indid = sik.indid
+                                           INNER JOIN
+                                       dbo.syscolumns sc ON sc.id = sik.id AND sc.colid = sik.colid
+                                           INNER JOIN
+                                       dbo.sysobjects so ON so.name = so.name AND so.xtype = 'PK'
+                                  WHERE sc.id = a.id
+                                    AND sc.colid = a.colid) THEN '√'
+                             ELSE '' END                                       AS 主键,
+                         b.name                                                AS 类型,
+                         a.length                                              AS 长度,
+                         COLUMNPROPERTY(a.id, a.name,
+                                        'PRECISION')                           AS 精度,
+                         ISNULL(COLUMNPROPERTY(a.id, a.name, 'Scale'), 0)      AS 小数位数,
+                         CASE WHEN a.isnullable = 1 THEN '√' ELSE '' END       AS 允许空,
+                         ISNULL(e.text, '')                                    AS 默认值,
+                         ISNULL(g.value, '')                                   AS 字段说明,
+                         d.crdate                                              AS 创建时间,
+                         CASE WHEN a.colorder = 1 THEN d.refdate ELSE NULL END AS 更改时间
+FROM dbo.syscolumns AS a
+         LEFT OUTER JOIN
+     dbo.systypes AS b ON a.xtype = b.xusertype
+         INNER JOIN
+     dbo.sysobjects AS d ON a.id = d.id AND d.xtype = 'U' AND d.status >= 0
+         LEFT OUTER JOIN
+     dbo.syscomments AS e ON a.cdefault = e.id
+         LEFT OUTER JOIN
+     sys.extended_properties AS g ON a.id = g.major_id AND a.colid = g.minor_id
+         LEFT OUTER JOIN
+     sys.extended_properties AS f ON d.id = f.major_id AND f.minor_id = 0
+ORDER BY d.name, 字段序号";
             try
             {
                 SqlDataAdapter da = new SqlDataAdapter(strQry, cnnString);
@@ -101,24 +127,19 @@ namespace DataDictionaryGenerator
                 foreach (DataRow dr in dtInfo.Rows)
                 {
                     string tableName = dr["表名"].ToString();
-                    
-                    if (dr["表名"] != DBNull.Value && !string.IsNullOrEmpty(dr["表名"].ToString()))
+                    if (!templateDatas.ContainsKey(tableName))
                     {
-                        if (!templateDatas.ContainsKey(tableName))
-                        {
-                            templateDatas.Add(tableName,new List<TableColumnInfo>());
-                        }
-
-                        templateDatas[tableName].Add(new TableColumnInfo()
-                        {
-                            AllowNull = dr["允许空"].ToString(),
-                            Description = dr["字段说明"].ToString(),
-                            IsPrimary = dr["主键"].ToString(),
-                            Name = dr["字段名"].ToString(),
-                            Type = dr["类型"].ToString(),
-                            TableName = tableName
-                        });
+                        templateDatas.Add(tableName,new List<TableColumnInfo>());
                     }
+                    templateDatas[tableName].Add(new TableColumnInfo()
+                    {
+                        AllowNull = dr["允许空"].ToString(),
+                        Description = dr["字段说明"].ToString(),
+                        IsPrimary = dr["主键"].ToString(),
+                        Name = dr["字段名"].ToString(),
+                        Type = dr["类型"].ToString(),
+                        TableName = tableName
+                    });
                 }
                 
                 
