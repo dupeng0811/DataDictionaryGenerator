55e8c8bbca5781fa7f83df85e0f983c76970743d
dupeng0811 <dupeng0811@qq.com>
2021-04-16 11:17:56 +0800

    Add project files.

---
+++ DataDictionaryGenerator/Model/TemplateData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DataDictionaryGenerator.Model
+{
+    public class TemplateData
+    {
+        public string TableName { get; set; }
+        public List<TableColumnInfo> TableColumnInfos { get; set; }
+    }
+
+
+    public class TableColumnInfo
+    {
+        public int Index { get; set; }
+        public string TableName { get; set; }
+        public string Name { get; set; }
+        public string IsPrimary { get; set; }
+        public string Type { get; set; }
+        public string AllowNull { get; set; }
+        public string Description { get; set; }
+    }
+}
